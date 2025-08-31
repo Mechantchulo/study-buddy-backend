a2ca562488ece064b46b0a16f7c565835a1e1b0f
Mechantchulo <chulomechant@gmail.com>
2025-08-31 22:49:09 +0300

    Settings configuration of main app

--- app/core/config.py
+++ app/core/config.py
@@ -1,30 +1,30 @@
 from pydantic_settings import BaseSettings
 from typing import Optional
 
-
 class Settings(BaseSettings):
-    #Database
+    # Database
     supabase_url: str
     supabase_key: str
     
-    #security
+    # Security
     secret_key: str
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
     
-    #AI services
+    # AI Services
     huggingface_api_key: str
     
-    #Payments
+    # Instasend Payments
     instasend_api_key: str
     instasend_secret_key: str
+    instasend_callback_url: Optional[str] = "https://webhook.site/d86478da-55f3-41e0-ada1-1595eb6bb61e"
     
-    #App
-    app_names: str = "Study Buddy"
+    # App
+    app_name: str = "AI Study Buddy"
     version: str = "1.0.0"
     debug: bool = True
     
     class Config:
         env_file = ".env"
-        
+
 settings = Settings()
